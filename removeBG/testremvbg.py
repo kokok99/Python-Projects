d2e6da8395619c3f3eba4643845f46f10cb23e77
unknown <mohdfitri1017@gmail.com>
2022-10-16 05:44:03 +0800

    add removeBG

---
+++ removeBG/testremvbg.py
@@ -0,0 +1,7 @@
+from rembg import remove
+from PIL import Image
+input_path = 'cat.jpg'
+output_path = 'output1.png'
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
